161023d1be515ee517d4683ee4f57a261d62359a
hh65 <hh65@sussex.ac.uk>
2025-06-30 12:03:38 +0100

    chore: add userfacing function to __init__.py

--- src/omero_screen_plots/__init__.py
+++ src/omero_screen_plots/__init__.py
@@ -1,6 +1,21 @@
 __version__ = "0.1.2"
 
+from omero_screen_plots.cellcycleplot import (
+    cellcycle_grouped,
+    cellcycle_plot,
+    cellcycle_stacked,
+)
+
 from .config import set_env_vars
 
 # Initialize environment variables
 set_env_vars("omero_screen_plots")
+
+# Import user-facing plot functions
+
+
+__all__ = [
+    "cellcycle_plot",
+    "cellcycle_stacked",
+    "cellcycle_grouped",
+]
